f4e934dfadd18dcdc165a3686b07d8154a6a849f
Dmeowmixer <ransonikuo@gmail.com>
2019-01-25 11:22:05 -1000

    adds materialize css npm package

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
-
-const Header = () => <h2>Header</h2>
+import Header from './Header.js'
 const Dashboard = () => <h2>Dashboard</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
@@ -11,8 +10,10 @@ const App = () => {
     <div>
       <BrowserRouter>
         <div>
+          <Header />
           <Route exact path="/" component={Landing} />
-          <Route path="/surveys" component={Dashboard} />
+          <Route exact path="/surveys" component={Dashboard} />
+          <Route path="/surveys/new" component={SurveyNew} />
         </div>
       </BrowserRouter>
     </div>
