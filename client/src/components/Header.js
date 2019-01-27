ca36688814dbc1fd4607f0c55b9b78766448c456
Dmeowmixer <ransonikuo@gmail.com>
2019-01-27 13:18:40 -1000

    still working on state management in header.js, found auth issue with /auth/google "violates CSP, priority"

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,11 +5,15 @@ class Header extends Component {
   renderContent() {
     switch( this.props.auth ){
       case null:
-        return 'Still deciding';
+        return;
       case false:
-        return 'I"m logged out'
+        return (
+          <li><a href="/auth/google">Login With Google</a></li>
+        )
       default:
-        return "I'm logged in";
+        return(
+          <li><a href="#">Logout</a></li>
+        );
     }
   }
   render() {
