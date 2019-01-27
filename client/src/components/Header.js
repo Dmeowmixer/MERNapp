094c5d1efec259b360e85910e3d49c5c1b0a891d
Dmeowmixer <ransonikuo@gmail.com>
2019-01-27 13:14:15 -1000

    rendering different content based on state in header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,31 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch( this.props.auth ){
+      case null:
+        return 'Still deciding';
+      case false:
+        return 'I"m logged out'
+      default:
+        return "I'm logged in";
+    }
+  }
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
           <a href="#" className="left brand-logo">Emaily</a>
           <ul id="nav-mobile" className="right">
-            <li><a href="sass.html">Login With Google</a></li>
+            {this.renderContent()}
           </ul>
         </div>
       </nav>
     );
   }
 }
-
-export default Header;
+function mapStateToProps({auth}){
+  return{ auth };
+}
+export default connect(mapStateToProps)(Header);
