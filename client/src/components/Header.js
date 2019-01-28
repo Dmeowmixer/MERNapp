f1b0c05dd3da0bff00342c92f3b8c360a61e4ba1
Dmeowmixer <ransonikuo@gmail.com>
2019-01-28 10:31:35 -1000

    adding frontend routes and react router Link to

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,26 +1,23 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-
+import {Link} from 'react-router-dom'
 class Header extends Component {
   renderContent() {
     switch( this.props.auth ){
       case null:
         return;
       case false:
-        return (
-          <li><a href="/auth/google">Login With Google</a></li>
-        )
+        return <li><a href="/auth/google">Login With Google</a></li>;
       default:
-        return(
-          <li><a>Logout</a></li>
-        );
+        return <li><a href="/api/logout">Logout</a></li>;
     }
   }
+
   render() {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a className="left brand-logo">Emaily</a>
+          <Link to={this.props.user ? '/surveys' : '/'} className="left brand-logo">Emaily</Link>
           <ul id="nav-mobile" className="right">
             {this.renderContent()}
           </ul>
---
+++ client/src/components/Landing.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{textAlign: 'center'}}>
+      <h1>
+        GP
+      </h1>
+      GoalPillar Steps/Milestones/Goals
+    </div>
+  )
+}
+export default Landing;
