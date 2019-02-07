c6d6e0fcd5c54e6ff1e4801d2e1e8c16764987df
Dmeowmixer <ransonikuo@gmail.com>
2019-02-06 14:56:07 -1000

    refactoring sendgrid in mailer/survey routes, adds survey template and binds

--- routes/surveryRoutes.js
+++ routes/surveryRoutes.js
@@ -5,7 +5,7 @@ const Mailer = require('../services/Mailer');
 const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req,res) => {
+  app.post('/api/surveys', requireLogin, requireCredits, async(req,res) => {
     const{ title, subject, body, recipients } = req.body;
 
     const survey = new Survey({
@@ -17,6 +17,15 @@ module.exports = app => {
       dateSent: Date.now()
     });
     const mailer = new Mailer(survey, surveyTemplate(survey));
-    mailer.send();
+    try{
+      await mailer.send();
+      await survey.save();
+
+      req.user.credits -= 1;
+      const user = await req.user.save();
+      res.send(user);
+    }catch(err){
+      res.status(422);
+    }
   });
 }
